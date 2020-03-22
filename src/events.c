161ad65765ee931a3b52ded2eef7e57aa52cb3b9
Smeglosiv <robiin-98@hotmail.com>
2020-03-22 20:38:07 +0100

    show window,handle close event

---
+++ src/events.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <SDL.h>
+#include "main.h"
+
+void HandleEvents()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT) 
+        {
+            running = 0;
+            SDL_Quit();
+            SDL_Log("Program quit after %i ticks", event.quit.timestamp);
+            break;
+        }
+    }
+}
--- src/main.c
+++ src/main.c
@@ -2,24 +2,26 @@
 #include <stdbool.h>
 
 #include <SDL.h>
+#include "events.h"
 
+int running = 1;
 int main(int argc, const char *argv[])
 {
-  printf("Corona Royale\n");
+    printf("Corona Royale\n");
 
-  if(!SDL_Init(0))
-  {
-    printf("Could not initialize SDL2");
-  }
+    if(!SDL_Init(0))
+    {
+      printf("Could not initialize SDL2");
+    }
 
-  SDL_ShowSimpleMessageBox(0, "Corona Royale", "It's corona time!", NULL);
+    SDL_ShowSimpleMessageBox(0, "Corona Royale", "It's corona time!", NULL);
+    SDL_Window* window = SDL_CreateWindow("Corona Royale",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1000,1000,0);
+    while (running)
+    {
+      HandleEvents();
+    }
 
-  // while (true)
-  // {
-    
-  // }
-
-  SDL_Quit();
-  return 0;
+    SDL_Quit();
+    return 0;
 }
 
