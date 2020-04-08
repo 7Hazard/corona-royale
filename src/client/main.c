88c16b821d98a0bc7b5b060a3ac3ee9884951ff4
Hazard <hazard9909@gmail.com>
2020-04-08 21:32:59 +0200

    Add some forgotten changes

--- src/client/main.c
+++ src/client/main.c
@@ -22,34 +22,11 @@ int main(int argc, const char *argv[])
 
     Game* game = GetGame();
 
-    //// EXAMPLE
     FC_Font* font = FC_CreateFont();  
     FC_LoadFont(font, game->renderer, "res/fonts/ComicSansMS3.ttf", 20, FC_MakeColor(255, 255, 255, 255), TTF_STYLE_BOLD|TTF_STYLE_ITALIC);
-    //// EXAMPLE
 
     while (game->running)
     {
-        // if (IsPlayerMoving(player) == false)
-        // {
-        //     HandleEvents(&player);
-
-        //     //SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        //     if (frameTime == 60)
-        //     {
-        //         frameTime = 0;
-        //         player.rect.x += frameWidth;
-        //         if (player.rect.x >= textureWidth)
-        //         {
-        //             player.rect.x = 0;
-        //         }
-                
-        //     }
-
-        //     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        // }
-        
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-
         frameStart = SDL_GetTicks();
 
         { /////////// STATE UPDATES PHASE BEGIN ///////////
@@ -63,24 +40,15 @@ int main(int argc, const char *argv[])
             
             // Draw background
             SDL_RenderCopy(game->renderer, game->background, &game->player.cameraRect, NULL);
-            
-            // if(DoBoxesIntersect(&player.rect, &player2.rect) && player2.infected != true)
-            // {
-            //     Mix_PlayChannel(-1, audio->cough, 0);
-            //     player2.infected = true;
-            // }
 
             OnPlayerRender(&game->player);
-            //OnPlayerRender(&player2);
 
-            //// EXAMPLE
-            {
+            { // Draw CORONA ROYALE text
                 static Uint8 r = 0;
                 static float theta = 0.f; theta+=0.03f;
                 r = ((sin(theta)+1)/2)*255;
                 FC_DrawColor(font, game->renderer, 200, 50, FC_MakeColor(r, 20, 20, 255), "CORONA\n%s", "ROYALE");
             }
-            //// EXAMPLE
 
             SDL_RenderPresent(game->renderer);
         } /////////// RENDERING PHASE END ///////////
@@ -92,12 +60,8 @@ int main(int argc, const char *argv[])
         }
     }
 
-    //// EXAMPLE
     FC_FreeFont(font);
-    //// EXAMPLE
 
-    // SDL_DestroyTexture(currentImage);
-    // SDL_DestroyTexture(background);
     IMG_Quit();
     SDL_Quit();
     StopAudio();
