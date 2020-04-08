39a83f64c69cfe1784520bd932b5bb30f36b6f6a
Hazard <hazard9909@gmail.com>
2020-04-08 05:50:01 +0200

    Added SDL2_TTF and SDL_FontCache

---
+++ deps/SDL2_ttf/include/SDL_ttf.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d8812df3f415637d49ea647d66d10d4ba7cead505445a5d8e6d089a92052d58b
+size 13072
--- src/client/main.c
+++ src/client/main.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_FontCache.h>
 
 #include "collision.h"
 #include "events.h"
@@ -19,6 +21,11 @@ int main(int argc, const char *argv[])
     int frameTime;
 
     Game* game = GetGame();
+
+    //// EXAMPLE
+    FC_Font* font = FC_CreateFont();  
+    FC_LoadFont(font, game->renderer, "res/fonts/ComicSansMS3.ttf", 20, FC_MakeColor(255, 255, 255, 255), TTF_STYLE_BOLD|TTF_STYLE_ITALIC);
+    //// EXAMPLE
 
     while (game->running)
     {
@@ -45,25 +52,38 @@ int main(int argc, const char *argv[])
 
         frameStart = SDL_GetTicks();
 
-        HandleEvents();
-        OnPlayerUpdate(&game->player);
+        { /////////// STATE UPDATES PHASE BEGIN ///////////
+            HandleEvents();
+            OnPlayerUpdate(&game->player);
+        } /////////// STATE UPDATES PHASE END /////////////
         
-        SDL_SetRenderDrawColor(game->renderer, 0, 0, 0, 255);
-        SDL_RenderClear(game->renderer);
-        
-        // Draw background
-        SDL_RenderCopy(game->renderer, game->background, &game->player.cameraRect, NULL);
-        
-        // if(DoBoxesIntersect(&player.rect, &player2.rect) && player2.infected != true)
-        // {
-        //     Mix_PlayChannel(-1, audio->cough, 0);
-        //     player2.infected = true;
-        // }
+        { /////////// RENDERING PHASE BEGIN /////////
+            SDL_SetRenderDrawColor(game->renderer, 0, 0, 0, 255);
+            SDL_RenderClear(game->renderer);
+            
+            // Draw background
+            SDL_RenderCopy(game->renderer, game->background, &game->player.cameraRect, NULL);
+            
+            // if(DoBoxesIntersect(&player.rect, &player2.rect) && player2.infected != true)
+            // {
+            //     Mix_PlayChannel(-1, audio->cough, 0);
+            //     player2.infected = true;
+            // }
 
-        OnPlayerRender(&game->player);
-        //OnPlayerRender(&player2);
-        
-        SDL_RenderPresent(game->renderer);
+            OnPlayerRender(&game->player);
+            //OnPlayerRender(&player2);
+
+            //// EXAMPLE
+            {
+                static Uint8 r = 0;
+                static float theta = 0.f; theta+=0.03f;
+                r = ((sin(theta)+1)/2)*255;
+                FC_DrawColor(font, game->renderer, 200, 50, FC_MakeColor(r, 20, 20, 255), "CORONA\n%s", "ROYALE");
+            }
+            //// EXAMPLE
+
+            SDL_RenderPresent(game->renderer);
+        } /////////// RENDERING PHASE END ///////////
 
         frameTime = SDL_GetTicks() - frameStart;
         if (frameDelay > frameTime)
@@ -71,6 +91,10 @@ int main(int argc, const char *argv[])
             SDL_Delay(frameDelay - frameTime);
         }
     }
+
+    //// EXAMPLE
+    FC_FreeFont(font);
+    //// EXAMPLE
 
     // SDL_DestroyTexture(currentImage);
     // SDL_DestroyTexture(background);
