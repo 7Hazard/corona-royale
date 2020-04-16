5eb74fffc5a0b618dbbd8cefc8611e252b54e7da
Contagious97 <62553422+Contagious97@users.noreply.github.com>
2020-04-16 22:11:41 +0200

    Update main.c

--- src/client/main.c
+++ src/client/main.c
@@ -22,24 +22,35 @@ int main(int argc, const char *argv[])
 
     while (game->running)
     {
-        
+<<<<<<< Updated upstream
+=======
         frameStart = SDL_GetTicks();
 
-        HandleEvents();
-        OnPlayerUpdate(&game->player);
+        { /////////// STATE UPDATES PHASE BEGIN ///////////
+            HandleEvents();
+            OnPlayerUpdate(&game->player);
+        } /////////// STATE UPDATES PHASE END /////////////
         
-        SDL_SetRenderDrawColor(game->renderer, 0, 0, 0, 255);
-        SDL_RenderClear(game->renderer);
-        
-        // Draw background
-        OnPlayerRender(&game->player);
-        SDL_RenderCopy(game->renderer, game->background, &game->player.camera.cameraRect, NULL);
-        
+        { /////////// RENDERING PHASE BEGIN /////////
+            SDL_SetRenderDrawColor(game->renderer, 0, 0, 0, 255);
+            SDL_RenderClear(game->renderer);
+            
+            // Draw background
+            SDL_RenderCopy(game->renderer, game->background, &game->player.camera.cameraRect, NULL);
 
-        OnPlayerRender(&game->player);
-        
-        SDL_RenderPresent(game->renderer);
+            OnPlayerRender(&game->player);
 
+            { // Draw CORONA ROYALE text
+                static Uint8 r = 0;
+                static float theta = 0.f; theta+=0.03f;
+                r = ((sin(theta)+1)/2)*255;
+                FC_DrawColor(font, game->renderer, 200, 50, FC_MakeColor(r, 20, 20, 255), "CORONA\n%s", "ROYALE");
+            }
+
+            SDL_RenderPresent(game->renderer);
+        } /////////// RENDERING PHASE END ///////////
+>>>>>>> Stashed changes
+        
         frameTime = SDL_GetTicks() - frameStart;
         if (frameDelay > frameTime)
         {
