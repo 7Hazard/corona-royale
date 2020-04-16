dde7650d48ba4da855b1c24ba88dcd8faf4610bd
Contagious97 <62553422+Contagious97@users.noreply.github.com>
2020-04-16 22:28:38 +0200

    updated main

--- src/client/main.c
+++ src/client/main.c
@@ -27,11 +27,7 @@ int main(int argc, const char *argv[])
 
     while (game->running)
     {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Camera
+
         frameStart = SDL_GetTicks();
 
         { /////////// STATE UPDATES PHASE BEGIN ///////////
@@ -54,18 +50,9 @@ int main(int argc, const char *argv[])
                 r = ((sin(theta)+1)/2)*255;
                 FC_DrawColor(font, game->renderer, 200, 50, FC_MakeColor(r, 20, 20, 255), "CORONA\n%s", "ROYALE");
             }
-<<<<<<< HEAD
-=======
 
             SDL_RenderPresent(game->renderer);
         } /////////// RENDERING PHASE END ///////////
-        
-        
->>>>>>> Camera
-
-            SDL_RenderPresent(game->renderer);
-        } /////////// RENDERING PHASE END ///////////
->>>>>>> Stashed changes
         
         frameTime = SDL_GetTicks() - frameStart;
         if (frameDelay > frameTime)
